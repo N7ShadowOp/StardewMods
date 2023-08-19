b17b3758793579304cf928b3dccb0c407f12456e
N7ShadowOp <N7ShadowOpGaming@gmail.com>
2023-08-19 08:06:19 -0800

    Mod Update

---
+++ [MP] Integrated Minecarts/assets/Tilesheets/mine_desert.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="untitled tile sheet" tilewidth="16" tileheight="16" tilecount="288" columns="16">
+ <image source="mine_desert.png" width="256" height="288"/>
+ <tile id="138">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+ <tile id="153">
+  <properties>
+   <property name="Type" value="Stone"/>
+  </properties>
+ </tile>
+</tileset>
